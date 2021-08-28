b5ef1dddca978bc190bab3979283c9b991e11c59
EOC10912TC06\tcuser <jack_chen@turn2cloud.com>
2021-08-29 00:46:08 +0800

    [RedmineId]: <Description>
    [Link]:
    [Versions]:
    [ReviewedBy]: <PeerName>

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Jack new 3rd commit!");
-        }
+            Console.WriteLine("Hello World Jack new 4th commit!");
+       
     }
 }
